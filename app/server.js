24e0022d71dbac83710ce4f8b4cabb62b79fb614
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2025-01-13 13:45:46 +0100

    feat(server): switch to HTTPS

--- app/server.js
+++ app/server.js
@@ -1,13 +1,18 @@
 const express = require("express");
-
+const https = require("https");
+const fs = require("fs");
 
 const app = express();
+
+const options = {
+    key: fs.readFileSync("certs/key.pem"),
+    cert: fs.readFileSync("certs/cert.crt")
+};
+
 const userRoute = require('./routes/User');
 app.use('/user', userRoute);
 
-
-
 // Démarrage du serveur
-app.listen(443, () => {
-    console.log('Server running on port 443');
+https.createServer(options, app).listen(443, () => {
+    console.log("Server running on port https://localhost:443");
 });
