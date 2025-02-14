4a66dc77d750db08df7da839d94189b2b103b15c
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2025-02-14 09:28:10 +0100

    refactor(api): change route to /api/users/

--- app/server.js
+++ app/server.js
@@ -25,7 +25,7 @@ const options = {
 };
 
 const userRoute = require("./routes/User");
-app.use("/user", userRoute);
+app.use("/api/users", userRoute);
 
 app.use(express.static(path.join(__dirname, "public")));
 
